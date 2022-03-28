18d1d96779f554b0379c41385c8b2ac4de05de60
doanthaitan1807 <doanthaitan123@gmail.com>
2022-03-28 16:53:10 +0700

    seeder, factories, faker

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => rand(1,11),
+            'title' => $this->faker->sentence(),
+            'description' => $this->faker->sentence(),
+            'start_time' => $this->faker->dateTime(),
+            'end_time' => $this->faker->dateTime(),
+            'status' => rand(0,1),
+        ];
+    }
+}
