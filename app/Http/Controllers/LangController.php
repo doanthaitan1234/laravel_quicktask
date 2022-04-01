c94977a60d5ee972a150bb823e1e5685c244fa91
doanthaitan1807 <doanthaitan123@gmail.com>
2022-04-01 16:22:21 +0700

    setup Localization

---
+++ app/Http/Controllers/LangController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Contracts\Session\Session;
+
+class LangController extends Controller
+{
+    private $langActive = [
+        'vi',
+        'en',
+    ];
+    
+    public function changeLanguage($language)
+    {
+       if (in_array($language, $this->langActive)) {
+           Session::put('website_language', $language);
+           
+           return redirect()->back();
+       }
+    }
+}
